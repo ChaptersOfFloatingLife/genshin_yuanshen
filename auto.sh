91b19e95cc7592057e5ffe14c7a1b85114f7c287
bamao88 <wuxiangping0313@gmail.com>
2025-02-09 22:15:05 +0800

    Update auto shell to auto list roles
    Signed-off-by: bamao88 <wuxiangping0313@gmail.com>

--- auto.sh
+++ auto.sh
@@ -3,14 +3,14 @@
 set -e  # 如果某个命令失败，立即退出脚本
 
 # 设置起始日期 (YYYY-MM-DD)
-start_date="2025-02-08"
+start_date="2025-02-10"
 start_time="18:00"
 
-# 正确的 bash 数组声明语法
-# characters=("Albedo_阿贝多" "Scaramouche_散兵" "wanye_万叶" "Xiao_魈")
-characters=("Scaramouche_散兵" "wanye_万叶" "Xiao_魈")
+# 获取resource目录下所有.json文件
+resources=($(ls resource/*.json | sed 's/\.json$//'))
 
-for character in "${characters[@]}"; do
+for resource in "${resources[@]}"; do
+    character=$(basename $resource)
     resource_path="resource/${character}.json"
     image_path="resource/${character}.jpg"
     
@@ -24,9 +24,8 @@ for character in "${characters[@]}"; do
     fi
 
     publish_datetime="$current_date $start_time"
+    echo "处理角色: $character"
     echo "使用图片: $image_path"
-
-    # continue
 
     cp $image_path output/tmp_image.jpg
     python scripts/produce_with_gpt.py $resource_path
